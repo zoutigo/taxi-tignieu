967e409bf85137042429c0ddec41d81dac74c810
zoutigo <zoutigo@gmail.com>
2025-11-11 21:55:28 +0100

    setup: eslint & prettier + cutomer & BookingStatus models

--- app/page.tsx
+++ app/page.tsx
@@ -14,20 +14,20 @@ export default function Home() {
           height={20}
           priority
         />
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx files or 
-            not.
+            To get started, edit the page.tsx files or not to implement.
           </h1>
-            <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle className="text-slate-900">Test Shadcn UI</CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-2">
-          <Button>Réserver</Button>
-          <Button variant="outline">Annuler</Button>
-        </CardContent>
-      </Card>
+          <Card className="w-[350px]">
+            <CardHeader>
+              <CardTitle className="text-slate-900">Test Shadcn UI</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2">
+              <Button>Réserver</Button>
+              <Button variant="outline">Annuler</Button>
+            </CardContent>
+          </Card>
           <p>La grande classe</p>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -37,14 +37,11 @@ export default function Home() {
             >
               Templates
             </a>{" "}
-            
-
-
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Learning
+              Learning he name
             </a>{" "}
             center.
           </p>
